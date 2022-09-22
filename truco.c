98f6a592bacabe67e7195757df2aaba415296c02
Juan Victor Aleixo <juanvaleixo@gmail.com>
2022-09-22 13:14:44 -0300

    Upload das primeiras structs
    Adicionando structs para definir os jogadores e as cartas dos jogadores, talvez isso mude no futuro.

--- truco.c
+++ truco.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+typedef struct TCarta{
+    char nome[1];
+    char naipe[10];  
+}Cartas;
+typedef struct TJogador{
+    int id;
+    Cartas Cs[3];
+}Jogador;
+
 int main(){
-    printf("truco\n");
-    printf("6 LADRAO\n");
+    int i;   
+    Jogador jogador1;
+    jogador1.id = 1;
+    strcpy(jogador1.Cs[0].nome,"K");
+    strcpy(jogador1.Cs[1].nome,"J");
+    strcpy(jogador1.Cs[2].nome,"5");
+    strcpy(jogador1.Cs[0].naipe,"Copas");
+    strcpy(jogador1.Cs[1].naipe,"Ouros");
+    strcpy(jogador1.Cs[2].naipe,"Espadas");
+
+    printf("Jogador %d\n",jogador1.id);
+    for(i=0; i < 3; i++){
+        printf("Carta %d\n",i+1);
+        printf("Valor: %s Naipe: %s\n",jogador1.Cs[i].nome,jogador1.Cs[i].naipe);
+    }
     return 0;
 }
